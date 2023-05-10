d3956222e495e89c31e63381517592cdde2b6fd1
Josef Forkman <strand.vatten@outlook.com>
2023-05-10 09:00:24 +0200

    comment mobile

--- app/commpnents/comment.tsx
+++ app/commpnents/comment.tsx
@@ -5,7 +5,7 @@ import Vote from "./vote";
 import { Comment, Reply } from "../interface/form-comment";
 
 export default function ({ comment }: { comment: Comment | Reply }) {
-    
+
     return (
         <div className="comment bg-Neutral-White">
             <div className="header">
@@ -15,20 +15,20 @@ export default function ({ comment }: { comment: Comment | Reply }) {
                     <span className="bg-Primary-Moderate-blue text-Neutral-White">you</span>
                     <p className="text-Neutral-Dark-blue">{comment.createdAt}</p>
                 </div>
-                <div className="right-side">
-                    <button className="btn-icon text-Primary-Soft-Red text-Primary-Pale-red-hover">
-                        <FontAwesomeIcon icon={faTrash} />
-                        <p>Delete</p>
-                    </button>
-                    <button className="btn-icon text-Primary-Moderate-blue text-Primary-Light-grayish-blue-hover">
-                        <FontAwesomeIcon icon={faPen} />
-                        <p>Edit</p>
-                    </button>
-                    <button className="btn-icon text-Primary-Moderate-blue text-Primary-Light-grayish-blue-hover">
-                        <FontAwesomeIcon icon={faReply} />
-                        <p>Reply</p>
-                    </button>
-                </div>
+            </div>
+            <div className="right-side">
+                <button className="btn-icon text-Primary-Soft-Red text-Primary-Pale-red-hover">
+                    <FontAwesomeIcon icon={faTrash} />
+                    <p>Delete</p>
+                </button>
+                <button className="btn-icon text-Primary-Moderate-blue text-Primary-Light-grayish-blue-hover">
+                    <FontAwesomeIcon icon={faPen} />
+                    <p>Edit</p>
+                </button>
+                <button className="btn-icon text-Primary-Moderate-blue text-Primary-Light-grayish-blue-hover">
+                    <FontAwesomeIcon icon={faReply} />
+                    <p>Reply</p>
+                </button>
             </div>
             <div className="body text-Neutral-Grayish-Blue">
                 <p>{comment.content}</p>
